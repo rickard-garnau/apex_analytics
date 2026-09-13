2c3f6a34f275ba7fa8d4b23b7769189bdf6a67a8
rickardgarnau-byte <rickardgarnau@gmail.com>
2026-09-13 12:50:02 +0200

    feat: extract api

--- src/apex_analytics/dlt_code/load_api.py
+++ src/apex_analytics/dlt_code/load_api.py
@@ -1,18 +1,35 @@
+import dlt
 import requests
 import json
+import httpx
 
 url = "https://api.jolpi.ca/ergast/f1/2026/races.json"
 
-def _get_races():
+def get_races():
     headers = {'accept': 'application/json'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, timeout=30)
     response.raise_for_status()
     return response.json()
 
-def number_of_races():
-    json_response = _get_races()
-    number_of_races = json_response['MRData']['total']
-    print(f"\nNumber of races = {number_of_races}")
+@dlt.resource(write_disposition="replace")
+def view_races():
+    json_response = get_races()
+    total = json_response['MRData']['RaceTable']['Races']
+    print(f"\nRaces = {total}")
+    yield total
+
+def run_pipeline(view_races): 
+    pipeline = dlt.pipeline(
+        pipeline_name ="apex_analytics",
+        destination="snowflake",
+        dataset_name="staging"
+    )
+
+    load_races = pipeline.run(view_races)
+    print(load_races)
+
+
 
 if __name__ == '__main__':
-    number_of_races()
+    run_pipeline(view_races)
+
